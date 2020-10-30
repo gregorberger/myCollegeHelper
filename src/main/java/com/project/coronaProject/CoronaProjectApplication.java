09638f19f4fc91a165e70295f322ffcb35346638
gregorberger <gregorberger1997@gmail.com>
2020-10-30 18:26:23 +0100

    Initial commit

---
+++ src/main/java/com/project/coronaProject/CoronaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.coronaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/coronaProject/CoronaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.coronaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
