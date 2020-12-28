3767252844a80e930aa81e385b8454d04d34106a
GregorBerger <gregorberger1997@gmail.com>
2020-12-28 16:10:25 +0100

    notes rest api

--- src/main/java/com/project/myCollegeHelper/restApi/RestController.java
+++ src/main/java/com/project/myCollegeHelper/restApi/RestController.java
@@ -1,9 +1,11 @@
 package com.project.myCollegeHelper.restApi;
 
+import com.project.myCollegeHelper.entity.SubjectNotesEntity;
 import com.project.myCollegeHelper.entity.SubjectsEntity;
 import com.project.myCollegeHelper.service.StudentServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -19,5 +21,10 @@ public class RestController {
     @RequestMapping(value = "/subject/all", method = RequestMethod.GET)
     ArrayList<SubjectsEntity> allSubjects() {
         return studentService.getAllSubjects();
+    }
+
+    @RequestMapping(value = "/subject/getNotes/{studentId}/{subjectId}", method = RequestMethod.GET)
+    ArrayList<SubjectNotesEntity> getNotes(@PathVariable String studentId, @PathVariable String subjectId) {
+        return studentService.getNotes(studentId, subjectId);
     }
 }
